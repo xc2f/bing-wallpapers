005f7c267fe2d4ad0921ee62e8ec61987fdd2081
Fen <hellowd93@163.com>
2026-03-31 01:57:25 +0800

    Revert "test build"
    This reverts commit 4f9753282b3b125444931379f93430818e89428f.

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ const url = new URL(process.env.BASE_URL || "https://www.bing.com")
 
 const nextConfig: NextConfig = {
   images: {
-    loader: "custom",
-    loaderFile: "./image-loader.ts",
     formats: ["image/avif", "image/webp"],
     minimumCacheTTL: 60 * 60 * 24,
     remotePatterns: [
